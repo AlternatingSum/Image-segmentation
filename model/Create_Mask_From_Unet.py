a2a04df02970ff9bdd8fd35e56185aae2f04e26a
Tova Brown <tova.h.f.brown@gmail.com>
2019-11-23 20:47:06 -0800

    Changed to use floats for original prediction

--- model/Create_Mask_From_Unet.py
+++ model/Create_Mask_From_Unet.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Uses a U-Net to refine a predicted segmentation mask
+Uses a regional U-Net to refine a predicted segmentation mask
 
 @author: tova
 """
 
 from keras.models import load_model
 
-import Masked_Image as process
+import Masked_image_motorcycle as process
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-import skimage
 from skimage import io
 
 # Loads the u-net weights
-unet = load_model('../data/model_weights/unet_16filters_32_dist.h5')
+unet = load_model('../data/model_weights/unet_16filters_32_dist_float_15epochs.h5')
 
 # The u-net takes square regions of size (2*buffer)x(2*buffer) as input
 buffer = 32
@@ -30,6 +29,7 @@ sigma = 8
 
 def create_gaussian(dist, sigma):
     """Creates a gaussian filter of size (2*dist)x(2*dist), with standard deviation sigma."""
+    
     gauss_array = np.zeros((2*dist, 2*dist))
     
     for row in range(2*dist):
@@ -37,7 +37,7 @@ def create_gaussian(dist, sigma):
             radius_squared = math.sqrt((row-dist)**2 + (col-dist)**2)
             gaussian = math.exp(-radius_squared/(2*sigma))
             gauss_array[row][col] = gaussian
-    
+            
     return gauss_array
 
 
@@ -55,7 +55,6 @@ def find_iou(correct_file, predicted_mask):
     # Scales the correct mask and predicted mask so that they are both arrays of 1s and 0s
     if np.amax(correct_mask) > 0.0:
         correct_mask = correct_mask/np.amax(correct_mask)
-    
     if np.amax(predicted_mask) > 0.0:
         predicted_mask = predicted_mask/np.amax(predicted_mask)
     
@@ -67,21 +66,22 @@ def find_iou(correct_file, predicted_mask):
         return 0.0
     else:
         return np.sum(intersection)/np.sum(union)
+    
 
-
-def create_new_mask(prefix):
+def create_mask(prefix):
     """Uses the u-net to create a new mask for the image represented by the prefix."""
     
-    # Creates paths for the image, 
+    # Creates paths for the image
     image_file = "../data/test/images/" + prefix + "_image.jpg"
     tuned_prediction_file = "../data/test/tuned_masks/" + prefix + "_.jpg"
     first_prediction_file = "../data/test/first_masks/" + prefix + "_.jpg"
-    correct_mask_file = "../data/test/correct_masks/" + prefix + "_mask.png"
+    float_prediction_file = "../data/test/float_masks/" + prefix + "_.jpg"
+    correct_mask_file = "../data/test/masks/" + prefix + "_mask.png"
     
     # Processes the image and adds padding
-    motorcycle = process.Masked_Image(image_file, tuned_prediction_file, tuned_prediction_file)
+    motorcycle = process.Masked_Image(image_file, tuned_prediction_file, float_prediction_file, tuned_prediction_file)
     motorcycle.add_padding(2*buffer, 2*buffer)
-    
+
     # Creates a list of all boundary points in the image
     boundary_list = motorcycle.list_boundary()
     
@@ -94,32 +94,42 @@ def create_new_mask(prefix):
     # This allows us to take a weighted average after obtaining all the data, 
     # by dividing the first entry by the second entry. 
     
-    # Modifies the prediction in a region around each boundary point
+    # Creates an array to store the input data for each region centered at a boundary point
+    region_array = np.zeros((len(boundary_list), 2*buffer, 2*buffer, 4))
+    boundary_index = 0
+
     for boundary_point in boundary_list:
         row = boundary_point[0]
         col = boundary_point[1]
-        
-        # Uses the u-net to make a prediction in the region around the boundary point
+    
         region = motorcycle.find_subregion(row, col, buffer)
-        local_prediction = unet.predict(np.expand_dims((region[:,:,:4]), axis=0))
-        
-        # Adds the weighted prediction to the first entry (in dimension = 2)
-        prediction_array[row-buffer:row+buffer, col-buffer:col+buffer,0] += local_prediction[0,:,:,0] * gaussian
-        
-        # Adds the weight to the second entry (in dimension = 2)
+        region_array[boundary_index, :, :, :] = region[:,:,:4]
+    
+        boundary_index += 1
+    
+    # Uses the u-net to make new predictions for each region, and combines them to make a new mask
+    total_prediction = unet.predict(region_array)
+    boundary_index = 0
+
+    for boundary_point in boundary_list:
+        row = boundary_point[0]
+        col = boundary_point[1]
+    
+        local_prediction = total_prediction[boundary_index, :, :, 0]
+    
+        prediction_array[row-buffer:row+buffer, col-buffer:col+buffer,0] += local_prediction * gaussian
         prediction_array[row-buffer:row+buffer, col-buffer:col+buffer,1] += gaussian
+    
+        boundary_index += 1
         
     # Scales the prediction array so it doesn't overwhelm the initial prediction
-    prediction_array = prediction_array / 6
-    
-    # Blurs the initial predicted foreground, so there aren't sharp changes in its predictions
-    motorcycle.predicted_foreground = skimage.filters.gaussian(motorcycle.predicted_foreground, sigma = sigma, multichannel=False)
+    prediction_array = prediction_array / 20
     
     # Incorporates the initial predicted foreground into the prediction array
-    prediction_array[:,:,0] += motorcycle.predicted_foreground
+    prediction_array[:,:,0] += motorcycle.float_foreground
     prediction_array[:,:,1] += np.ones((height, width))
     
-    # Sets all 0s equal to 1 in the second entry in dimension=2, to avoid dividing by 0 in the next step
+    # Sets all 0s equal to 1 in the second entry in dimension 2, to avoid dividing by 0 in the next step
     prediction_array[:,:,1] += (prediction_array[:,:,1] == 0.0)
     
     # Divides the sum of all weighted predictions by the sum of all weights, 
@@ -138,18 +148,22 @@ def create_new_mask(prefix):
     first_predicted_mask = io.imread(first_prediction_file)
     
     # Prints the IoUs for all three masks
-    print("IoU for the first predicted mask:")
+    print("The IoU for the initial prediction was:")
     print(find_iou(correct_mask_file, first_predicted_mask))
-    print("IoU for the fine tuned mask:")
+    print("The IoU for the fine tuned prediction was:")
     print(find_iou(correct_mask_file, tuned_predicted_mask))
-    print("IoU for the new mask, created by the u-net:")
+    print("The IoU for the u-net's prediction is:")
     print(find_iou(correct_mask_file, new_prediction))
+    print()
+    print("Here is the u-net's prediction:")
     
-    return(new_prediction)
+    return new_prediction
+
 
 # Creates the gaussian filter
 gaussian = create_gaussian(buffer, sigma)
 
 # Creates a new mask for an image. Change the prefix to see a different image. 
-current_prefix = "t_41"
-create_new_mask(current_prefix)
+# Use a prefix in data/test/first_masks/ 
+prefix = "t_571"
+new_prediction = create_mask(prefix)
